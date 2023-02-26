fce879e3822497df6c4128af21052e32b7358d9b
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 12:20:03 +1300

    Add mock stripe price objrect to msw handlers

--- client/src/__mocks__/handlers.js
+++ client/src/__mocks__/handlers.js
@@ -1,5 +1,6 @@
 import { rest } from 'msw';
 
+// ** mocks external api data
 export const getMockStripeResponse = [
   {
     id: 'prod_NP6tgOqEeyQ7HH',
@@ -26,16 +27,46 @@ export const getMockStripeResponse = [
     url: null,
   },
 ];
-
+const getMockStripePrice =  [
+    {
+      id: "price_1MeIfWCDSrHORaZX7lcKvh2S",
+      object: "price",
+      active: true,
+      billing_scheme: "per_unit",
+      created: 1677073658,
+      currency: "nzd",
+      custom_unit_amount: null,
+      livemode: false,
+      lookup_key: null,
+      metadata: {},
+      nickname: null,
+      product: "prod_NP6tgOqEeyQ7HH",
+      recurring: null,
+      tax_behavior: "unspecified",
+      tiers_mode: null,
+      transform_quantity: null,
+      type: "one_time",
+      unit_amount: 7900,
+      unit_amount_decimal: "7900",
+    },
+];
+  
+// ** mocks internalapi data
 const postApiMsgMockResponse = 'Message was posted!! 🛎️';
 const getApiMsgMockResponse = 'Message was fetched!! 🛎️';
 
 export const handlers = [
+  //** intercepts internal server api endpoints
   rest.post(`${process.env.REACT_APP_API_URL}/test`, (req, res, ctx) => {
     return res(ctx.json(postApiMsgMockResponse));
   }),
   rest.get(`${process.env.REACT_APP_API_URL}/test`, (req, res, ctx) => {
     return res(ctx.json(getApiMsgMockResponse));
+  }),
+
+  //** intercepts external server api endpoints
+  rest.get(`${process.env.REACT_APP_API_URL}/test`, (req, res, ctx) => {
+    return res(ctx.json(getMockStripePrice));
   }),
   rest.get(`${process.env.REACT_APP_API_URL}/products`, (req, res, ctx) => {
     return res(ctx.json(getMockStripeResponse));

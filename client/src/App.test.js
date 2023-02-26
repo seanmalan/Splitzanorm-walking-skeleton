72d335938c1faf4021757cdbe2d79cb53d205ac5
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 11:53:49 +1300

    Setup msw test file in src folder

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { getMockStripeResponse as mockProduct } from './mocks/handlers';
+import { render, screen, waitFor, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  beforeEach(() => {
+    // ** what goes here?
+  });
+
+  test('The app renders as expected', () => {
+    expect(screen.getByText(/products/i)).toBeInTheDocument();
+  });
+
+  test('All stripe product elements are rendered', async () => {
+    render(<App />);
+    await waitFor(() => {
+      mockProduct.forEach((product) => {
+        expect(
+          screen.getByRole('cell', { name: product.description })
+        ).toBeInTheDocument();
+      });
+    });
+  });
+
+  test('A stripe product is added to cart', async () => {
+    await waitFor(() => {
+      mockProduct.forEach((product) => {
+        expect(
+          screen.getByRole('cell', { name: product.description })
+        ).toBeInTheDocument();
+      });
+    });
+
+    const stripeProduct = screen
+      .getByText(mockProduct.description)
+      .closest('div');
+
+    const buyNowButton = within(stripeProduct).getByRole('button', {
+      name: /buy now/i,
+    });
+    userEvent.click(buyNowButton);
+
+    await waitFor(() => {
+      expect(
+        screen.queryByRole('cell', { name: mockProduct.description })
+      ).not.toBeInTheDocument();
+    });
+  });
 });

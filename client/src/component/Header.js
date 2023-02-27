fde987d69597234a77bb9e59527b349c910bdf3c
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 16:24:39 +1300

    Tidy header css to correspond to App.css file

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,40 +1,26 @@
-import React from "react";
-import { BsBag } from "react-icons/bs";
-import Logo from "../public/img/Logo.png";
+import React from 'react';
+import { BsBag } from 'react-icons/bs';
+import '../App.css';
 
 
-const Header = () => {
-
-
-
-
+const div = () => {
   return (
     <>
-    <header className="fixed w-full bg-cover bg-gradient-to-r from-teal-200 to-cyan-200 px-[30px] lg:px-[100px] lf:h-[140px] flex-items items-center">
-        <div>
-            <img className="center-item" src={Logo} alt="logo" />
-          </div>
-
-        <nav className="hidden xl:flex gap-x-12 font-semibold">
-        <div>
-        <a
-            href="https://buy.stripe.com/test_eVa3d253o51a5Ww144"
-            target="_blank"
-            rel="noopener noreferrer"
+      <header className='App-header'>
+        <p></p>
+        <img className="center-item App-logo" src="logo.png" alt="logo" />
+        <nav className=''>
+          <a
+            href='https://buy.stripe.com/test_eVa3d253o51a5Ww144'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-                    <BsBag className='text-2xl' />
-            </a>
-        </div>
+            <BsBag className='text-2xl' />
+          </a>
         </nav>
-
-        <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-black rounded-full flex justify-center items-center">
-
-        </div>
-      
-
-    </header>
+      </header>
     </>
   );
 };
 
-export default Header;
+export default div;

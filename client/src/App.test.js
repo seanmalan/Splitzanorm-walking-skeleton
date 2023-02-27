3a8ecdd3d76615d336dd19da924d8e7b3deef7d5
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 16:39:20 +1300

    Create App.test.js file for development

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,51 +1,63 @@
 import React from 'react';
-import { getMockStripeResponse as mockProduct } from './mocks/handlers';
-import { render, screen, waitFor, within } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import // getMockStripeProduct as mockProduct,
+// getMockStripePrice as mockPrice,
+// postToApiMsg,
+// getFromApiMsg,
+'./mocks/handlers';
+import {
+  screen,
+  render,
+  // waitFor,
+  // within
+} from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
   beforeEach(() => {
-    // ** what goes here?
+    // ** ??
+  });
+  afterEach(() => {
+    // ** ??
   });
 
   test('The app renders as expected', () => {
+    render(<App />);
     expect(screen.getByText(/products/i)).toBeInTheDocument();
   });
 
-  test('All stripe product elements are rendered', async () => {
-    render(<App />);
-    await waitFor(() => {
-      mockProduct.forEach((product) => {
-        expect(
-          screen.getByRole('cell', { name: product.description })
-        ).toBeInTheDocument();
-      });
-    });
-  });
+  // test('All product elements are rendered', async () => {
+  //   render(<App />);
+  //   await waitFor(() => {
+  //     mockProduct.forEach((product) => {
+  //       expect(
+  //         screen.getByRole('', { name: product.description })
+  //       ).toBeInTheDocument();
+  //     });
+  //   });
+  // });
 
-  test('A stripe product is added to cart', async () => {
-    await waitFor(() => {
-      mockProduct.forEach((product) => {
-        expect(
-          screen.getByRole('cell', { name: product.description })
-        ).toBeInTheDocument();
-      });
-    });
+  // test('A stripe product is...', async () => {
+  //   await waitFor(() => {
+  //     mockProduct.forEach((product) => {
+  //       expect(
+  //         screen.getByRole('cell', { name: product.description })
+  //       ).toBeInTheDocument();
+  //     });
+  //   });
 
-    const stripeProduct = screen
-      .getByText(mockProduct.description)
-      .closest('div');
+  //   const stripeProduct = screen.getByText(mockProduct.description);
+  //   expect(stripeProduct).toHaveTextContent('');
 
-    const buyNowButton = within(stripeProduct).getByRole('button', {
-      name: /buy now/i,
-    });
-    userEvent.click(buyNowButton);
+  //   const buyNowButton = within(stripeProduct).getByRole('button', {
+  //     name: /buy now/i,
+  //   });
+  //   userEvent.click(buyNowButton);
 
-    await waitFor(() => {
-      expect(
-        screen.queryByRole('cell', { name: mockProduct.description })
-      ).not.toBeInTheDocument();
-    });
-  });
+  //   await waitFor(() => {
+  //     expect(
+  //       screen.queryByRole('cell', { name: mockProduct.description })
+  //     ).not.toBeInTheDocument();
+  //   });
+  // });
 });

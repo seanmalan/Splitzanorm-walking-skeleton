ec39913e697db44c4df2bc075d418bdd6f1b9666
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 16:34:46 +1300

    Create and comment out formatPrice component which inclides intl formatter in services for development

---
+++ client/src/services/formatPrice.js
@@ -0,0 +1,11 @@
+// const formatPrice = (Price) => {
+//   const numberFormat = new Intl.NumberFormat('en-NZ', {
+//     style: 'currency',
+//     currency: 'NZD',
+//     currencyDisplay: 'symbol',
+//   });
+//   const total = Price / 100;
+//   return numberFormat.format(total);
+// };
+ 
+// export default formatPrice;

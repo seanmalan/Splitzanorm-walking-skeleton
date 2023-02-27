eeb5617f0f4e9870d98c7c1c8981bd447b15530b
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 16:36:41 +1300

    Set up and comment out Header test file

--- client/src/component/Header.test.js
+++ client/src/component/Header.test.js
@@ -1,15 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
+// import { render, screen } from '@testing-library/react';
+// import Header from './Header';
 
-describe('Header', () => {
-
-  test('The header renders', () => {
-    render(
-        <Header />
-    );
-
-    expect(screen.getByText(/products/i)).toBeInTheDocument();
-    expect(screen.getByText(/cloche/i)).toBeInTheDocument();
-    expect(screen.getByText(/splitzanorm/i)).toBeInTheDocument();
-  });
-});
+// describe('Header', () => {
+//   test('The header renders', () => {
+//     render(<Header />);
+//     expect(screen.getByText(/products/i)).toBeInTheDocument();
+//     expect(screen.getByText(/cloche/i)).toBeInTheDocument();
+//     expect(screen.getByText(/splitzanorm/i)).toBeInTheDocument();
+//   });
+// });

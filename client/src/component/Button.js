63079f23aa92cf49606463be25e6aeb09005787c
jennifer corbett <jennnifer.corbett@developersinstitute.co.nz>
2023-02-27 16:27:31 +1300

    Create and comment out buttons for testing internal api development

--- client/src/component/Button.js
+++ client/src/component/Button.js
@@ -4,29 +4,30 @@
 // } from 'react'
 // import api from "../api"
 
-
-// const ApiPOSTButton = () => {
+// const ApiPostButton = () => {
 // const [apiMsg, setApiMsg] = useState(null)
 
 //     const handleApiMsg = async () => {
 //     const response = await addApiMsg()
 //     setApiMsg(alert(response));
 //     };
-  
+
 //   return (
 
 //   )
 // }
 
-// const ApiGETButton = () => {
+// const ApiGetButton = () => {
 // const [apiGetMsg, setApiGetMsg] = useState(null)
 
 //     const handleApiMsg = async () => {
 //     const response = await getApiMsg()
 //     setApiGetMsg(alert(response));
 //     };
-  
+
 //   return (
 
 //   )
 // }
+
+// export const Button = { ApiGETButton, ApiPostButton }
